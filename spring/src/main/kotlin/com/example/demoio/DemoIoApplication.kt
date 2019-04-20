285670d3d69d0ea4b6e4e6a9e61a5c67bf3b669e
Sebastiano Poggi <poggos@gmail.com>
2019-04-20 09:01:47 +0100

    Reformat DemoIoApplication

--- spring/src/main/kotlin/com/example/demoio/DemoIoApplication.kt
+++ spring/src/main/kotlin/com/example/demoio/DemoIoApplication.kt
@@ -7,19 +7,20 @@ import org.springframework.cloud.gcp.data.datastore.repository.DatastoreReposito
 import org.springframework.cloud.gcp.storage.GoogleStorageResource
 import org.springframework.context.ApplicationContext
 import org.springframework.core.io.Resource
-import org.springframework.core.io.WritableResource
 import org.springframework.data.annotation.Id
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import org.springframework.http.*
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import java.io.InputStream
-import java.io.OutputStream
 import java.util.*
-import java.util.concurrent.TimeUnit
-import javax.servlet.http.HttpServletResponse
-import javax.websocket.server.PathParam
 
 @SpringBootApplication
 class DemoIoApplication
@@ -33,10 +34,11 @@ class UploadController(
         private val applicationContext: ApplicationContext,
         private val photoRepository: PhotoRepository
 ) {
+
     private val prefix = "gs://cloud-kotlin-io19/demo"
 
     @PostMapping("/upload")
-    fun upload(@RequestParam("file") file : MultipartFile): Photo {
+    fun upload(@RequestParam("file") file: MultipartFile): Photo {
         val id = UUID.randomUUID().toString()
         val fileUri = "$prefix/$id"
 
@@ -55,7 +57,7 @@ class UploadController(
     }
 
     @GetMapping("/image/{id}")
-    fun image(@PathVariable id : String) : ResponseEntity<Resource> {
+    fun image(@PathVariable id: String): ResponseEntity<Resource> {
         val resource = applicationContext.getResource("$prefix/$id")
 
         val headers = HttpHeaders()
@@ -65,7 +67,7 @@ class UploadController(
     }
 
     @DeleteMapping("/image/{id}")
-    fun deleteImage(@PathVariable id : String) : ResponseEntity<String> {
+    fun deleteImage(@PathVariable id: String): ResponseEntity<String> {
         val resource = applicationContext.getResource("$prefix/$id") as GoogleStorageResource
         return if (resource.exists()) {
             photoRepository.deleteById(id)
@@ -82,7 +84,7 @@ class UploadController(
 interface PhotoRepository : DatastoreRepository<Photo, String>
 
 @Entity
-data class Photo (
+data class Photo(
         @Id
         var id: String? = null,
         var uri: String? = null,
